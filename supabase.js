3ffc1ce721a7230bd8c13bf644f1998440c29a53
Jessica Corpuz <corpuz.201800213@gmail.com>
2026-06-25 06:39:57 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkdHdiaWl1aW1iaG96ZHFyY3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTE5NDIsImV4cCI6MjA5NzM4Nzk0Mn0.8qDaxtDB7H0lr3bkIdZpMIC7Mwqx2BtMXiIbGIv8fa0'
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
